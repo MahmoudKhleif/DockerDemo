2299c8d46810f4044a3e83132f16d6d3a500b620
Mahmoud Khleif <Mahmoud89ah@gmail.com>
2022-01-02 22:27:21 +0100

    Add project files.

---
+++ DockerDemo/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace DockerDemo
+{
+    public class Class1
+    {
+        public int GiveMeFive()
+        {
+            return 5;
+        }
+    }
+}
---
+++ DockerDemoTests/Class1Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DockerDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockerDemo.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void GiveMeFiveTest()
+        {
+            var expected = 5;
+            var actual = new Class1().GiveMeFive();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
